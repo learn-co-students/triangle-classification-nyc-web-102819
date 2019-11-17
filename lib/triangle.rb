c085c53be941cd311c9f3005ec29ad097b650f85
Pavel Beletsky <pavel.beletsky@live.com>
2019-11-16 20:29:03 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,39 @@
 class Triangle
-  # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    if (@a+@b <= @c) || (@a+@c <= @b) || (@c+@b <= @a) || @a <= 0 || @b <= 0 || @c <= 0
+      
+      raise TriangleError
+      
+      # begin
+      #   raise TriangleError
+      # rescue TriangleError => error
+      #   puts error.message
+      # end
+    else
+      if @a == @b && @b == @c
+        :equilateral
+      elsif @a == @b || @a == @c || @b == @c
+        :isosceles
+      else
+        :scalene
+      end
+    end
+  end
+
+
+  class TriangleError < StandardError
+    def message
+      "Invalid input"
+    end
+  end
+
 end
